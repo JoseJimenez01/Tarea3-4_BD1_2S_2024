e9f3fba0c3b1b391ca808039a31e4fb162b542db
JoseJimenez01 <jg3633110@gmail.com>
2024-10-21 22:02:17 -0600

    Primer Commit
    Publicando la branch en el main, y el proyecto en .net creado.

---
+++ Solucion_Tarea3-4_BD1/Tarea3-4_BD1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea3_4_BD1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
